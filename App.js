dfbb2bb02072f43d079c082d8e9b49e6246724ee
bengant3 <bengant3@gmail.com>
2021-08-10 18:19:28 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, Alert, Image, SafeAreaView } from 'react-native';
+// import {Image} from "react-native-web";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+        <Text style={styles.timerText}>25:00</Text>
+        {/*<StatusBar style={styles.goldenrod}>*/}
+        {/*    <Text >status bar</Text>*/}
+        {/*</StatusBar>*/}
+        <View
+            style = {{
+                flex: 1,
+                flexDirection: "row",
+            }}>
+            <Image source={require("./assets/icon.png")} />
+            <Image source={require("./assets/resetbutton.png")} />
+        </View>
+      <TouchableOpacity onPress={() =>
+          Alert.alert("Title", "Message")}>
+        <Image
+            source={{
+              width: 200,
+              height: 300,
+              uri: "https://picsum.photos/200/300",
+            }}
+        />
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'indianred',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    timerText: {
+        color: 'white',
+        fontSize: 44
+    },
+    goldenrod: {
+        backgroundColor: "#daa520"
+    }
 });
